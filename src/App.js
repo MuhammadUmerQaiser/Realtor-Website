1a5066e1b12ce301bee399e02700df6465fc1fd7
Muhammad Umer <muhammad.isl.umer123@gmail.com>
2023-02-17 16:56:30 +0500

    Install Router and create pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Offers from "./Pages/Offers";
+import SignIn from "./Pages/SignIn";
+import SignUp from "./Pages/SignUp";
+import ForgetPassword from "./Pages/ForgetPassword";
+import Profile from "./Pages/Profile";
+
 function App() {
   return (
-    <div className="text-2xl font-bold underline">
-      <h1>Welcome to Realtor App</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/offers" element={<Offers />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/forget-password" element={<ForgetPassword />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </Router>
   );
 }
 
