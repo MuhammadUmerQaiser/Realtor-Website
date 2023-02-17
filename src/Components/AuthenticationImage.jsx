99cd10e1e4d93c1043ee98e662334069750a874b
Muhammad Umer <muhammad.isl.umer123@gmail.com>
2023-02-17 21:26:35 +0500

    Half completed the sign in page

---
+++ src/Components/AuthenticationImage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AuthenticationImage() {
+  return (
+    <div>
+      <img src='https://images.unsplash.com/photo-1560518883-ce09059eeffa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8a2V5fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60' alt='House Image' className='w-full rounded-2xl' />
+    </div>
+  )
+}
